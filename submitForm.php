d3d597a52e242e08bc6324218dcb5456590bc7f3
Ben <benb803@hotmail.co.uk>
2023-05-06 12:48:57 +0100

    added trim, stripslashes and htmlspecialchars validation check

--- submitForm.php
+++ submitForm.php
@@ -10,57 +10,55 @@
     // ----------------  Setting the conn var with a mysqli object containing the config files ---------------- 
     $conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port);
 
+    // ----------------  Function that filters POST request data. Alters special chars, trims whitespace, tab, newline and strips backslashes ---------------- 
+    function validate_input($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+
     // Declaring vars from the AJAX POST request
-    $firstName = $_POST['fname'];
-    $lastName = $_POST['lname'];
-    $email = $_POST['email'];
-    $phoneNumber = $_POST['phone'];
-    $message = $_POST['message'];
+    $firstName = validate_input($_POST['fname']);
+    $lastName = validate_input($_POST['lname']);
+    $email = validate_input($_POST['email']);
+    $phoneNumber = validate_input($_POST['phone']);
+    $message = validate_input($_POST['message']);
 
     // ---------------- Backend form validation in case user turns of Javascript in the front end ---------------- 
     // Name and message validation
-    if ($firstName === '' || $lastName === '') {
-      $output['status']['code'] = "403";
-      $output['status']['name'] = "Name fields cannot be empty";
-      mysqli_close($conn);
-      echo json_encode($output);
-      exit;
+    $errors = [];
+    if (empty($firstName) || empty($lastName)) {
+      $errors[] = "Name fields cannot be empty";
     }
 
     // Message validation
-    if ($message === '') {
-      $output['status']['code'] = "403";
-      $output['status']['name'] = "Message field cannot be empty";
-      mysqli_close($conn);
-      echo json_encode($output);
-      exit;
+    if (empty($message)) {
+      $errors[] = "Message field cannot be empty";
     }
 
     // Phone validation
-    if ($phoneNumber === '') {
-      $output['status']['code'] = "403";
-      $output['status']['name'] = "Phone number field cannot be empty";
-      mysqli_close($conn);
-      echo json_encode($output);
-      exit;
+    if (empty($phoneNumber)) {
+      $errors[] = "Phone number field cannot be empty";
     }
 
     // Email validation
-    if ($email === '') {
-      $output['status']['code'] = "403";
-      $output['status']['name'] = "Email cannot be empty";
-      mysqli_close($conn);
-      echo json_encode($output);
-      exit;
-    } else {
-      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $output['status']['code'] = "403";
-      $output['status']['name'] = "Email format invalid";
-      mysqli_close($conn);
-      echo json_encode($output);
-      exit;
-      }
+    if (empty($email)) {
+      $errors[] = "Email cannot be empty";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Email format invalid";
     }
+
+    // If there are any errors, return the error response
+    if (!empty($errors)) {
+      $output['status']['code'] = "403";
+      $output['status']['name'] = "Error";
+      $output['status']['description'] = implode("; ", $errors);
+      $output['data'] = [];
+      mysqli_close($conn);
+      echo json_encode($output);
+      exit;
+  }
 
     // ----------------  Setting connection/database error response first before any process ---------------- 
     if (mysqli_connect_errno()) {
